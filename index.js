6a6d3e351709485d63369c475fae7c890da2de9e
camilo-a-g-z <cagarciaz@udistrital.edu.co>
2024-04-17 16:23:00 -0500

    Refactor Express server configuration and add route for retrieving users from Firebase

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import express, { json } from "express";
 //import { corsMiddleware } from "./middlewares/cors.js";
 //import { UserModel } from "./models/usuarios.js";
 import { db } from "./config/conf_firebase.js";
-import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { collection, addDoc, getDocs } from "firebase/firestore";
 export const createApp = () => {
-    const app = express();
+  const app = express();
 
-    app.use(json());
-    //app.use(corsMiddleware);
-    app.disable("x-powered-by");
+  app.use(json());
+  //app.use(corsMiddleware);
+  app.disable("x-powered-by");
 
-    app.get("/", async (req, res) => {
-        const user = await getDocs(collection(db, "Usuario"));
+  app.get("/", async (req, res) => {});
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
+
+  return app;
+};
+/*
+const user = await getDocs(collection(db, "Usuario"));
         res.json(user.docs.map((doc) => ({
             id: doc.id,
             ...doc.data()
-        })));
-    });
-
-    const PORT = process.env.PORT || 3000;
-
-    app.listen(PORT, () => {
-        console.log(`Server started on http://localhost:${PORT}`);
-    });
-
-    return app;
-}
+        })));  */
