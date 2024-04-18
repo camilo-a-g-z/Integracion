29d3cc04db5c710d4373080ffc1f678c1f236fd5
camilo-a-g <camilo@eurekadreams.com>
2024-04-17 21:26:31 -0500

    Add UnidadModel to handle CRUD operations for unidad collection

---
+++ models/unidad.js
@@ -0,0 +1,57 @@
+/**
+ * horEntSem: {
+    fin: string,
+    inicio: string
+  };
+  horFinSem: {
+    fin: string,
+    inicio: string
+  };
+  idUnidad: string;
+  nombre: string,
+  tMinPrestamo: string;
+ */
+import { db } from "../config/conf_firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+
+export class UnidadModel {
+  static async create({
+    horEntSem,
+    horFinSem,
+    idUnidad,
+    nombre,
+    tMinPrestamo,
+  }) {
+    const unidad = await db.collection("unidad").add({
+      horEntSem,
+      horFinSem,
+      idUnidad,
+      nombre,
+      tMinPrestamo,
+    });
+
+    return unidad.id;
+  }
+
+  static async getAll() {
+    const dataUnidades = [];
+    const unidades = await getDocs(collection(db, "unidad")).then(
+      (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          dataUnidades.push(doc.data());
+        });
+      }
+    );
+    return dataUnidades;
+  }
+
+  static async getByNombre(nombre) {
+    const unidades = await UnidadModel.getAll();
+    return unidades.find((unidad) => unidad.nombre === nombre) ?? null;
+  }
+
+  static async getById(idUnidad) {
+    const unidades = await UnidadModel.getAll();
+    return unidades.find((unidad) => unidad.idUnidad === idUnidad) ?? null;
+  }
+}
